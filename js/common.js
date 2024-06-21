7a574ad89bfd7e4944866215e4bb4baff272774e
曾家淳 <szjyp226@163.com>
2024-06-21 12:15:34 +0800

    updated

--- js/common.js
+++ js/common.js
@@ -42,22 +42,25 @@
 
           return;
         }*/
-        onFinishScriptList(totalLoadTimeout < 0.5 ? "Skip" : "");
+        const promise = onFinishScriptList(totalLoadTimeout < 0.5 ? "Skip" : "");
         console.log("加载耗时", totalLoadTimeout + "秒");
         window.setLoadingState = () => "";
+        return {
+          finish: callback => promise.then(() => callback && callback()),
+        };
       }
     };
 
-/*
-    int = setInterval(function () {
-      const len = getLoadingState();
-      if (len <= 70) {
-        return state(Math.floor(Math.random() * 10));
-      }
+    /*
+        int = setInterval(function () {
+          const len = getLoadingState();
+          if (len <= 70) {
+            return state(Math.floor(Math.random() * 10));
+          }
 
-      clearInterval(int);
-    }, 1000);
-*/
+          clearInterval(int);
+        }, 1000);
+    */
 
     return state;
   })();
@@ -75,54 +78,56 @@
   // script加载全部完成时候
   function onFinishScriptList(stepType) {
     // todo: 按钮3D化需要做
+    return new Promise(function (resolve) {
+      const mask = document.getElementById("CircleLoopMask");
+      const mask2 = document.getElementById("CircleLoopMask2");
+      const mask3 = document.getElementById("CircleLoopMask3");
+      const textContainer = document.getElementById("textContainer");
+      const appContainer = document.getElementById("AppContainer");
+      const maskContainer = mask.children[0];
+      const mask2Container = mask2.children[0];
+      const mask3Container = mask3.children[0];
+      let proxy = maskContainer;
 
-    const mask = document.getElementById("CircleLoopMask");
-    const mask2 = document.getElementById("CircleLoopMask2");
-    const mask3 = document.getElementById("CircleLoopMask3");
-    const textContainer = document.getElementById("textContainer");
-    const appContainer = document.getElementById("AppContainer");
-    const maskContainer = mask.children[0];
-    const mask2Container = mask2.children[0];
-    const mask3Container = mask3.children[0];
-    let proxy = maskContainer;
+      if (stepType === "Skip") {
+        proxy = mask3Container;
+      }
 
-    if (stepType === "Skip") {
-      proxy = mask3Container;
-    }
+      function _startupApplication() {
+        document.getElementById("LoadingSVG").style.display = "none";
+        document.body.classList.remove("overflow-hidden");
+        window.removeEventListener("resize", window.$$__ISUMI_SET_SVG_VIEW_BOX);
+        appContainer.classList.remove("body-container-animation");
+        delete window.$$__ISUMI_SET_SVG_VIEW_BOX;
+        document.getElementById("ScriptOtherDOM").remove();
+        resolve();
+      }
 
-    function _startupApplication() {
-      document.getElementById("LoadingSVG").style.display = "none";
-      document.body.classList.remove("overflow-hidden");
-      window.removeEventListener("resize", window.$$__ISUMI_SET_SVG_VIEW_BOX);
-      appContainer.classList.remove("body-container-animation");
-      delete window.$$__ISUMI_SET_SVG_VIEW_BOX;
-      document.getElementById("ScriptOtherDOM").remove();
-    }
+      if (AnimationIsClosed) {
+        return _startupApplication();
+      }
 
-    if (AnimationIsClosed) {
-      return _startupApplication();
-    }
-
-    // maskContainer容器的动画结束时
-    proxy.addEventListener("animationend", function _AnimationEnd(e) {
-      if (e.currentTarget === maskContainer) {
-        mask2Container.addEventListener("animationend", _AnimationEnd);
-        mask2Container.classList.add("start");
-        maskContainer.removeEventListener("animationend", _AnimationEnd);
-      } else if (e.currentTarget === mask2Container) {
-        mask3Container.addEventListener("animationend", _AnimationEnd);
-        mask3Container.classList.add("start");
+      // maskContainer容器的动画结束时
+      proxy.addEventListener("animationend", function _AnimationEnd(e) {
+        if (e.currentTarget === maskContainer) {
+          mask2Container.addEventListener("animationend", _AnimationEnd);
+          mask2Container.classList.add("start");
+          maskContainer.removeEventListener("animationend", _AnimationEnd);
+        } else if (e.currentTarget === mask2Container) {
+          mask3Container.addEventListener("animationend", _AnimationEnd);
+          mask3Container.classList.add("start");
+          appContainer.classList.add("start");
+          mask2Container.removeEventListener("animationend", _AnimationEnd);
+        } else if (e.currentTarget === mask3Container) {
+          _startupApplication();
+          mask3Container.removeEventListener("animationend", _AnimationEnd);
+        }
+      });
+      textContainer.classList.add("start");
+      proxy.classList.add("start");
+      if (proxy === mask3Container) {
         appContainer.classList.add("start");
-        mask2Container.removeEventListener("animationend", _AnimationEnd);
-      } else if (e.currentTarget === mask3Container) {
-        _startupApplication();
-        mask3Container.removeEventListener("animationend", _AnimationEnd);
       }
     });
-    textContainer.classList.add("start");
-    proxy.classList.add("start");
-    if (proxy === mask3Container) {
-      appContainer.classList.add("start");
-    }
   }
 })();
